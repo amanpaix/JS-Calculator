7161f8ea3e1d1880c0cef7536b8ef644b838fe36
Aman <usernameispaix@gmail.com>
2018-02-10 19:36:30 +0530

    Fixes all bug

--- js/script.js
+++ js/script.js
@@ -17,10 +17,31 @@ function checkFirstVal() {
     firstNum = parseFloat($(".form-control").val());
     $(".form-control").val("");
   } else {
-    operator = arr[i - 1];
-    equalPressed();
+    if($(".form-control").val() == "") {
+      var value = $(".history").val();
+      value = value.replace(value[value.length - 1] , arr[i]);
+      $(".history").val(value);
+      exit();
+    }
+    var op = arr[i - 1];
+    // equalPressed();
+    if(op == '+') {
+      firstNum = firstNum + parseFloat($(".form-control").val());
+      $(".form-control").val("");
+    } else if(op == '-') {
+      firstNum = firstNum - parseFloat($(".form-control").val());
+      $(".form-control").val("");
+    } else if(op == '*') {
+      firstNum = firstNum * parseFloat($(".form-control").val());
+      $(".form-control").val("");
+    } else if(op == '/') {
+      firstNum = firstNum / parseFloat($(".form-control").val());
+      $(".form-control").val("");
+    }
+
   }
 }
+
 function operatorPressed() {
   // TO DO
   console.log(arr);
@@ -79,6 +100,7 @@ function equalPressed() {
       break;
     default:
   }
+  $(".history").val(result);
   firstNum = null;
   secondNum = null;
   for(var i = 0; i < arr.length; i++) {
@@ -92,22 +114,51 @@ function delLastChar() {
   console.log("Delete Pressed");
   $(".form-control").focus();
   var value = $(".form-control").val();
+  var value1 = $(".history").val();
+
+  value1 = value1.split("");
   value = value.split("");
+
+  value1.pop();
   value.pop();
+
+  value1 = value1.join("");
   value = value.join("");
+
+  $(".history").val(value1);
   $(".form-control").val(value);
 }
 $(document).ready(function() {
   $(".form-control").val("");
+  $(".history").val("");
   $(".form-control").focus();
   $(".value").on("click" , valuePressed);
   $(".operator").on("click" , operatorPressed);
   $(".equal").on("click" , equalPressed);
   $(".delete").on("click" , delLastChar);
+  $(".square").on("click" , function() {
+    var value = parseFloat($(".form-control").val());
+    if($(".form-control").val() != "") {
+      $(".form-control").val(value * value);
+      $(".history").val(value * value);
+    }
+  });
+  $(".squareRoot").on("click" , function() {
+    var value = parseFloat($(".form-control").val());
+    value = Math.sqrt(value);
+    if($(".form-control").val() != "") {
+      $(".form-control").val(value);
+      $(".history").val(value);
+    }
+  });
+  $(".btn").on("click" , function() {
+    $(".history").val( $(".history").val() + this.value);
+  });
   $(".clear").on("click" , function() {
     firstNum = null;
     secondNum = null;
     $(".form-control").val("0");
+    $(".history").val("");
     $(".form-control").focus();
   });
 
